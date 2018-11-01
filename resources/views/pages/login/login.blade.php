774c4f1b47520eae344a2e037fb9dec0362098b4
memotiur <memotiur@gmail.com>
2018-11-01 21:34:14 +0600

    login error notification added

--- resources/views/pages/login/login.blade.php
+++ resources/views/pages/login/login.blade.php
@@ -18,6 +18,11 @@
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div class="card card-signin my-5">
                 <div class="card-body">
+
+                    @if(session('failed'))
+                        <p style="color: red;text-align: center">{{session('failed')}}!</p>
+                    @endif
+
                     <h5 class="card-title text-center">Sign In</h5>
                     <form class="form-signin" method="post" action="/login-check">
                         <div class="form-label-group">
